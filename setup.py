7506b8ccd717b0f490fb584cd8f76f79b9df0306
Jeff Simpson <jeff.simpson@livingsocial.com>
2013-03-05 13:35:37 -0700

    Disabled pyx compilation by default for distribution

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ Setup file for cylowess.pyx, a faster lowess smoother in Cython.
 try:
     from distutils.core import setup
     from distutils.extension import Extension
-    from Cython.Distutils import build_ext
     import numpy as np
+#    from Cython.Distutils import build_ext # uncomment for development
 
     setup(
         name='cylowess',
-        cmdclass = {'build_ext' : build_ext},
+#        cmdclass = {'build_ext' : build_ext}, # uncomment for development
         include_dirs = [np.get_include()],
-        ext_modules = [Extension('cylowess', ['cylowess.pyx'])]
+        ext_modules = [Extension('cylowess', ['cylowess.c'])]
         )
 except:
     pass
