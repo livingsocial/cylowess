09fd47067d2d9bdb9073578edd1b90597e97ac5f
Jeff Simpson <jeff.simpson@livingsocial.com>
2013-03-05 13:25:23 -0700

    Added pip requirements for Cython and numpy

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 '''
 Setup file for cylowess.pyx, a faster lowess smoother in Cython.
 '''
+try:
+    from distutils.core import setup
+    from distutils.extension import Extension
+    from Cython.Distutils import build_ext
+    import numpy as np
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-import numpy as np
-
-
-
-setup(
-    cmdclass = {'build_ext' : build_ext},
-    include_dirs = [np.get_include()],
-    ext_modules = [Extension('cylowess', ['cylowess.pyx'])]
-    )
-
+    setup(
+        name='cylowess',
+        cmdclass = {'build_ext' : build_ext},
+        include_dirs = [np.get_include()],
+        ext_modules = [Extension('cylowess', ['cylowess.pyx'])]
+        )
+except:
+    pass
