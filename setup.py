8357809a6f79d379d80ec4e93484d0c1c7ca6d55
Jeff Simpson <jeff.simpson@livingsocial.com>
2013-03-06 10:14:36 -0700

    Updated setup.py to remove try/except. Added note about required libraries to README

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 '''
 Setup file for cylowess.pyx, a faster lowess smoother in Cython.
 '''
-try:
-    from distutils.core import setup
-    from distutils.extension import Extension
-    import numpy as np
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy as np
 #    from Cython.Distutils import build_ext # uncomment for development
 
-    setup(
-        name='cylowess',
-        version='1.0.0',
-#        cmdclass = {'build_ext' : build_ext}, # uncomment for development
-        include_dirs = [np.get_include()],
-        ext_modules = [Extension('cylowess', ['cylowess.c'])]
-        )
-except:
-    pass
+setup(
+    name='cylowess',
+    version='1.0.0',
+    #        cmdclass = {'build_ext' : build_ext}, # uncomment for development
+    include_dirs = [np.get_include()],
+    ext_modules = [Extension('cylowess', ['cylowess.c'])]
+    )
